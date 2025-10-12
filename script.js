e56446b4925818122ab06ae90784a1fe099e70b6
komecafeofficial <komecafeofficial@gmail.com>
2025-10-12 18:28:34 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.getElementById('menuToggle');
+  const mainNav = document.getElementById('mainNav');
+
+  menuToggle.addEventListener('click', () => {
+    mainNav.classList.toggle('active');
+  });
+});
