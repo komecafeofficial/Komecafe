0ccc0eaeefd1b3a1ec27a49cdf8f8913ec70708e
komecafe <komecafeofficial@gmail.com>
2025-11-03 16:44:46 +0000

    Update README file

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align="center"><b>Kome Cafe</b></h1>
+
+<p align="center">
+  Your orders will be active very soon  
+  <br>
+  📞 Call <b>0541678088</b>
+</p>
--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const menuToggle = document.getElementById('menuToggle');
-  const mainNav = document.getElementById('mainNav');
-
-  menuToggle.addEventListener('click', () => {
-    mainNav.classList.toggle('active');
-  });
-});
